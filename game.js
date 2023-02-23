53219b27e77e0864d997697d4c88302e53791cde
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-02-24 01:50:09 +0200

    danger zone created

--- game.js
+++ game.js
@@ -4,10 +4,24 @@ window.onload=function(){
     start_div.addEventListener("click",reset);
     let test =document.getElementsByClassName("boundary")
     console.log(test)
+    test[0].addEventListener("mouseover",danger)
+    test[1].addEventListener("mouseover",danger)
+    test[2].addEventListener("mouseover",danger)
+    test[3].addEventListener("mouseover",danger)
+    test[4].addEventListener("mouseover",danger)
+    test[5].addEventListener("mouseover",danger)
+
+
+    
     function reset(){
        let i=0
        for(i=0;i<5;i++)
        test[i].style.backgroundColor="#eeeeee"
+           }
+    function danger(){
+       let i=0
+       for(i=0;i<5;i++)
+       test[i].style.backgroundColor="red"
            }
 
     }
