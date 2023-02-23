fa136b90937cf5a4747a5f55c2f16d9cbd42eceb
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-02-24 01:34:54 +0200

    reset finished

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
 window.onload=function(){
-    let start_div = document.getElementById("game")
-    console.log(start_div)
-    start_div.addEventListener("click",reset)
+    let start_div = document.getElementById("start");
+    console.log(start_div);
+    start_div.addEventListener("click",reset);
+    let test =document.getElementsByClassName("boundary")
+    console.log(test)
+    function reset(){
+       let i=0
+       for(i=0;i<5;i++)
+       test[i].style.backgroundColor="#eeeeee"
+           }
+
+    }
   
 
-   function reset(){
-    start_div.style.backgroundColor="red"
-   }
-}
+  
